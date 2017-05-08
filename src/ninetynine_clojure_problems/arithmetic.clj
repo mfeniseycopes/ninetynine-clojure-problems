0bd3bf23b7ab17896625f0ce44a82a0fe13b8549
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-05-08 18:58:32 -0400

    Add 32

--- src/ninetynine_clojure_problems/arithmetic.clj
+++ src/ninetynine_clojure_problems/arithmetic.clj
@@ -2,6 +2,10 @@
   (:use [clojure.math.numeric-tower])
   (:gen-class))
 
+(defn divides?
+  [divisee divisor]
+  (zero? (mod divisee divisor)))
+
 ;; 31 - is-prime?
 (defn is-prime?
   "Determine if a given integer is prime"
@@ -9,3 +13,13 @@
   (not-any? 
     #(and (not= potential %) (zero? (mod potential %)))
     (cons 2 (range 3 (inc (first (exact-integer-sqrt potential))) 2))))
+
+;; 32 - gcd
+(defn greatest-common-divisor 
+  "Find the greatest common denominator of two numbers"
+  [a b]
+  (if (> a b)
+    (recur b a)
+    (if (divides? b a)
+      a
+      (recur (- b a) a))))
--- test/ninetynine_clojure_problems/arithmetic_test.clj
+++ test/ninetynine_clojure_problems/arithmetic_test.clj
@@ -8,3 +8,9 @@
     (is (is-prime? 2))
     (not (is-prime? 4))
     (not (is-prime? 18))))
+
+(deftest greatest-common-divisor-test
+  (testing "#greatest-common-divisor"
+    (= (greatest-common-divisor 1 1) 1)
+    (= (greatest-common-divisor 100 125) 25)
+    (= (greatest-common-divisor 7 19) 1)))
